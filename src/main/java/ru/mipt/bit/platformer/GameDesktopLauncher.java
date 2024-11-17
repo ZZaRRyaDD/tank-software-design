7d0caa2e707667230b1427ce4b540dd3c31fd9ae
Nazar <furnoxl@yandex.ru>
2024-11-17 11:14:52 +0300

    change code for init entities from xml

--- src/main/java/ru/mipt/bit/platformer/GameDesktopLauncher.java
+++ src/main/java/ru/mipt/bit/platformer/GameDesktopLauncher.java
@@ -4,37 +4,21 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import static com.badlogic.gdx.graphics.GL20.GL_COLOR_BUFFER_BIT;
-import static ru.mipt.bit.platformer.util.GdxGameUtils.createSingleLayerMapRenderer;
-import static ru.mipt.bit.platformer.util.GdxGameUtils.getSingleLayer;
-
-import com.badlogic.gdx.maps.MapRenderer;
-import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
-import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.math.Interpolation;
 import ru.mipt.bit.platformer.entity.drawers.base.LevelGraphic;
 import ru.mipt.bit.platformer.entity.drawers.decorators.HealthBarDrawerDecorator;
 import ru.mipt.bit.platformer.entity.drawers.drawers.factories.BulletDrawerFactory;
 import ru.mipt.bit.platformer.entity.drawers.drawers.factories.ObstacleDrawerFactory;
 import ru.mipt.bit.platformer.entity.drawers.drawers.factories.TankDrawerFactory;
-import ru.mipt.bit.platformer.entity.listener.LevelListener;
 import ru.mipt.bit.platformer.entity.objects.Bullet;
 import ru.mipt.bit.platformer.entity.objects.Level;
-import ru.mipt.bit.platformer.entity.drawers.drawers.LevelDrawer;
 import ru.mipt.bit.platformer.entity.objects.Obstacle;
 import ru.mipt.bit.platformer.entity.objects.Tank;
 import ru.mipt.bit.platformer.entity.objects.base.GameObject;
 import ru.mipt.bit.platformer.entity.objects.generators.LevelGenerator;
-import ru.mipt.bit.platformer.entity.objects.generators.StrategyGenerate;
-import ru.mipt.bit.platformer.entity.objects.generators.from_file.parsers.LevelParser;
-import ru.mipt.bit.platformer.entity.objects.generators.from_file.parsers.plaint_text.PlainTextLevelParser;
-import ru.mipt.bit.platformer.entity.objects.generators.from_file.readers.LevelReader;
-import ru.mipt.bit.platformer.entity.objects.generators.from_file.readers.plaint_text.PlaintTextLevelReader;
 import ru.mipt.bit.platformer.entity.objects.generators.random.RandomLevelGenerator;
-import ru.mipt.bit.platformer.entity.objects.generators.from_file.FromFileLevelGenerator;
 import ru.mipt.bit.platformer.playerinput.actions.base.AbstractAction;
 import ru.mipt.bit.platformer.playerinput.inputs.ActionGenerator;
 import ru.mipt.bit.platformer.playerinput.inputs.InputActions;
@@ -48,9 +32,9 @@ import ru.mipt.bit.platformer.playerinput.inputs.player.DefaultPlayerInputAction
 import ru.mipt.bit.platformer.playerinput.inputs.player.PlayerInputActions;
 import ru.mipt.bit.platformer.playerinput.inputs.player.PlayerInput;
 import ru.mipt.bit.platformer.util.TileMovement;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 
@@ -68,31 +52,21 @@ public class GameDesktopLauncher implements ApplicationListener {
 
     @Override
     public void create() {
-        createLevelGraphic();
-        createLevel();
-    }
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-    public void createLevel() {
-        List<LevelListener> listeners = Arrays.asList(levelDrawer);
-        LevelGenerator levelGenerator = getLevelGeneratorStrategy(StrategyGenerate.RANDOM, listeners);
+        levelDrawer = context.getBean("mainLevelDrawer", HealthBarDrawerDecorator.class);
+        TiledMapTileLayer groundLayer = context.getBean("groundLayer", TiledMapTileLayer.class);
+        TileMovement tileMovement = new TileMovement(groundLayer, Interpolation.smooth);
+        initGraphicFactories(levelDrawer, tileMovement, groundLayer);
+
+        LevelGenerator levelGenerator = context.getBean("levelRandomGenerator", RandomLevelGenerator.class);
         levelGenerator.generate();
 
         level = levelGenerator.getLevel();
 
         initActionGenerators(levelGenerator, levelDrawer);
-    }
 
-    public void createLevelGraphic() {
-        TiledMap map = new TmxMapLoader().load("level.tmx");
-        Batch batch = new SpriteBatch();
-        MapRenderer renderer = createSingleLayerMapRenderer(map, batch);
-        TiledMapTileLayer groundLayer = getSingleLayer(map);
-        TileMovement tileMovement = new TileMovement(groundLayer, Interpolation.smooth);
-
-        levelDrawer = new LevelDrawer(map, renderer, batch);
-        levelDrawer = new HealthBarDrawerDecorator(levelDrawer);
-
-        initGraphicFactories(levelDrawer, tileMovement, groundLayer);
+        context.close();
     }
 
     public void initGraphicFactories(LevelGraphic levelDrawer, TileMovement tileMovement, TiledMapTileLayer groundLayer) {
@@ -136,22 +110,6 @@ public class GameDesktopLauncher implements ApplicationListener {
         InputActions graphicActions = new GraphicActions();
         new DefaultGraphicActions().registerActions(graphicActions);
         actionGenerators.add(new Graphic(graphicActions, levelDrawer));
-    }
-
-    public LevelGenerator getLevelGeneratorStrategy(StrategyGenerate strategy, List<LevelListener> listeners) {
-        LevelGenerator levelGeneratorStrategy;
-        switch (strategy) {
-            case FROM_FILE_PLAIN_TEXT:
-                LevelReader reader = new PlaintTextLevelReader("src/main/resources/level.txt");
-                LevelParser parser = new PlainTextLevelParser();
-                levelGeneratorStrategy = new FromFileLevelGenerator(reader, parser, listeners);
-                break;
-            case RANDOM:
-            default:
-                levelGeneratorStrategy = new RandomLevelGenerator(8, 10, listeners);
-                break;
-        }
-        return levelGeneratorStrategy;
     }
 
     @Override
